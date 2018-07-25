4acf458680f1766465921a3a1f99ecc250717b1b
fanhl <dyguests@qq.com>
2018-07-25 22:04:51 +0800

    listView

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
 //      home: new MainPage(title: 'Flutter Demo Home Page'),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          //child: Text(wordPair.asPascalCase), // Change the highlighted text to...
-          child: RandomWords(), // ... this highlighted text
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -112,15 +104,48 @@ class _MyHomePageState extends State<MainPage> {
 
 class RandomWords extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return RandomWordsState();
-  }
+  createState() => RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+//    final wordPair = WordPair.random();
+//    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
