695bd6f06703192a2bcc83c90a8fc8af60df4700
fanhl <dyguests@qq.com>
2018-07-26 08:05:12 +0800

    Photo

---
+++ lib/ui/main_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  createState() => MainPageState();
+}
+
+class MainPageState extends State<MainPage> {
+  final _populars = <Photo>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cure'),
+//        actions: <Widget>[IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(4.0),
+      itemBuilder: (context, i) {
+        return _buildRow(_populars[i]);
+      },
+    );
+  }
+
+  /// 创建每一项
+  Widget _buildRow(Photo photo) {}
+}
+
+class Photo {
+/*  {
+  photo: {
+  id: 6551257,
+  url: "/photo/6551257",
+  subject: "双黒",
+  sq150_url: "https://wc-ahba9see.c.sakurastorage.jp/469922/rjprqluawpxfuotgrjxldhmathkvhedofjwgnvoe-350x600.jpg",
+  sq300_url: "https://wc-ahba9see.c.sakurastorage.jp/469922/rjprqluawpxfuotgrjxldhmathkvhedofjwgnvoe-350x600.jpg",
+  does_like: false,
+  liked: false,
+  is_owner: false,
+  viewable: true
+  }
+  }*/
+
+  final int id;
+  final String url;
+  final String subject;
+  final String sq150_url;
+  final String sq300_url;
+  final bool does_like;
+  final bool liked;
+  final bool is_owner;
+  final bool viewable;
+}
