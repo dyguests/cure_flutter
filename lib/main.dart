c8ac1a112d35e2d584b648542a2df8140ebd18e0
fanhl <dyguests@qq.com>
2018-07-28 19:52:30 +0800

    remove app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
-import 'package:cure/app.dart';
+import 'package:cure/ui/main_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Cure Flutter',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(),
+    );
+  }
+}
